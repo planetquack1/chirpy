066e50409ab27874b53d860c15fef01679eefc82
Isaiah <irodol@uw.edu>
2024-08-27 18:13:01 -0700

    Added server to http://localhost:8080/

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
+
+	mu := http.NewServeMux()
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mu,
+	}
+
+	srv.ListenAndServe()
 
 }
